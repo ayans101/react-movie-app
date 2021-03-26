31c5ec524263ae6a1f2c744b124ef1d331875e01
Ayan Sarkar <ayansarkar367@gmail.com>
2021-03-27 02:00:54 +0530

    handle toggle between movies and favourites tabs

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@
 export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_TO_FAVOURITE = 'ADD_TO_FAVOURITE';
 export const REMOVE_FROM_FAVOURITE = 'REMOVE_FROM_FAVOURITE';
+export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
 
 //  action creators
 export function addMovies(movies) {
@@ -30,6 +31,14 @@ export function removeFromFavourite(movie) {
     return {
         type: REMOVE_FROM_FAVOURITE,
         movie
+    }
+
+}
+
+export function setShowFavourite(val) {
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val
     }
 
 }
